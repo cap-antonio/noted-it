466a0e7487d4aae2f58543308eef9677cb5add92
cap.Antonio <cheremnykh.anton@gmail.com>
2020-03-19 23:06:08 +0100

    Updates

---
+++ precache-manifest.f798ecae1d8b210d9ac3c45a1df311ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860b4aefacee3e8095f26c3e70d62001",
+    "url": "/noted-it/index.html"
+  },
+  {
+    "revision": "774c0f744af32b5cadbc",
+    "url": "/noted-it/static/css/2.a34aa59c.chunk.css"
+  },
+  {
+    "revision": "eab639a77b08e01afdeb",
+    "url": "/noted-it/static/css/main.09e55c20.chunk.css"
+  },
+  {
+    "revision": "774c0f744af32b5cadbc",
+    "url": "/noted-it/static/js/2.db14136d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/noted-it/static/js/2.db14136d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab639a77b08e01afdeb",
+    "url": "/noted-it/static/js/main.8e83d77e.chunk.js"
+  },
+  {
+    "revision": "b1ff2281da6c2bc46740",
+    "url": "/noted-it/static/js/runtime-main.131ee5e5.js"
+  }
+]);
