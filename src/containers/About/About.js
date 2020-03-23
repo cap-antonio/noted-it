1a221fbaf4e7a7c872636dc781f55dd38e953f99
cap.Antonio <cheremnykh.anton@gmail.com>
2020-03-23 09:31:21 +0100

    About page edited

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -11,6 +11,8 @@ const Main = styled.div`
   `
 const About = () => {
     const [localEditMode, setLocalEditMode] = useState(false)
+    const [showCard, setShowCard] = useState("d-none")
+    const [colorCard, setColorCard] = useState("card border-primary mb-3")
     const adder = (
         <button className="btn btn-primary float-left" onClick={() => setLocalEditMode(true)}>
             <i className="fas fa-plus-circle" />
@@ -33,36 +35,38 @@ const About = () => {
                                         disabled
                                         attributes={{ placeholder: 'about.placeholder' }} />
                                 </div>
-                                <button type="button" className="btn btn-primary btn-sm float-right">
+                                <button type="button" className="btn btn-primary btn-sm float-right"
+                                    onClick={() => setShowCard("col-sm-3 mb-3")}>
                                     <i className="fas fa-check-circle" />
                                 </button>
                             </div>
                         </form>
                         : adder}
                 </div>
-                <div className="col-sm-3 mb-3">
-                    <div className="card border-primary mb-3" id = "card-note">
-                    <div className="card-header">
-                        <Translate component={TextareaAutosize} disabled attributes={{ placeholder: 'about.noteText' }} />
-                    </div>
-                    <div>
-                        <i className="arrow fas fa-chevron-circle-up" />
-
-                        <div className="d-flex justify-content-center align-items-center" >
-                            <div id="color-icon" className="p-2 bd-highlight">
-                                <i className="green-icon fas fa-square f0c8" />
-                                <i className="red-icon fas fa-square f0c8" />
-                                <i className="grey-icon fas fa-square f0c8" />
-                                <i className="orange-icon fas fa-square f0c8" />
-                                <i className="yellow-icon fas fa-square f0c8" />
-                                <i className="blue-icon fas fa-square f0c8" />
-                                <i className="black-icon fas fa-square f0c8" />
-                                <i className="basic-icon far fa-square" />
-                            </div>
-                            <button id="trash" className="btn btn-primary btn-sm">
-                                <i className="fas fa-trash-alt" />
-                            </button>
+                <div className={showCard}>
+                    <div className={colorCard} id="card-note">
+                        <div className="card-header">
+                            <Translate component={TextareaAutosize} disabled attributes={{ value: 'about.noteText' }} />
                         </div>
+                        <div>
+                            <i className="arrow fas fa-chevron-circle-up" />
+
+                            <div className="d-flex justify-content-center align-items-center" >
+                                <div id="color-icon" className="p-2 bd-highlight">
+                                    <i className="green-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-success mb-3")} />
+                                    <i className="red-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-primary mb-3")} />
+                                    <i className="grey-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-secondary mb-3")} />
+                                    <i className="orange-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-danger mb-3")} />
+                                    <i className="yellow-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-warning mb-3")} />
+                                    <i className="blue-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-info mb-3")} />
+                                    <i className="black-icon fas fa-square f0c8" onClick={() => setColorCard("card text-white bg-dark mb-3")} />
+                                    <i className="basic-icon far fa-square" onClick={() => setColorCard("card border-primary mb-3")} />
+
+                            </div>
+                                <button id="trash" className="btn btn-primary btn-sm" onClick = {() => setShowCard("d-none")}>
+                                    <i className="fas fa-trash-alt" />
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/lang/en.js
+++ src/lang/en.js
@@ -4,7 +4,7 @@ export default {
     aboutPage: 'About',
     about: {
         placeholder: 'Here is a good place to note something about anything. When finish just click on the red button at the bottom right.',
-        textabout: `Welcome! That is a simple Note app. An interface contatin only few elements to manage your notes. Let's check it out below.`,
+        textabout: `Welcome! That is a simple Note app. An interface contatin only few elements to manage your notes. Let's check it out below. Press the Plus button.`,
         noteText: `That is your note looks like. To edit the note just edit it and click anywhere outside the note window. At the bottom, you will find a pane te set a color for your note. Enjoy!`
     }
 }
